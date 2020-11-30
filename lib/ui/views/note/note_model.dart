bf54480e623a8af484fceb53c3b53f4548fde85d
Sang Phạm Tài <phamtaisang97@gmail.com>
2020-11-30 09:13:29 +0700

    update data model

--- lib/ui/views/note/note_model.dart
+++ lib/ui/views/note/note_model.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class Note {
   /// id tự sinh ra ngẫu nhiên
-  final String id =
-  UniqueKey().hashCode.toUnsigned(20).toRadixString(16).padLeft(5, '0');
+  String id = UniqueKey()
+      .hashCode
+      .toUnsigned(20)
+      .toRadixString(16)
+      .padLeft(5, '0');
 
   final String title;
   final String desc;
@@ -19,7 +22,35 @@ class Note {
   /// trong CSDL mà không cần khởi tạo nên nó là static để dễ sử dụng.
   static String get createTable =>
       'CREATE TABLE $tableName(`id` TEXT PRIMARY KEY,'
-          ' `title` TEXT,'
-          ' `desc` TEXT,'
-          ' `isDeleted` INTEGER DEFAULT 0)';
+      ' `title` TEXT,'
+      ' `desc` TEXT,'
+      ' `isDeleted` INTEGER DEFAULT 0)';
+
+  /// Phương thức này được thiết lập để tạo nên danh sách các ghi chú
+  /// được lấy về từ CSDL, nó được tạo dưới dạng danh sách các ghi chú
+  /// theo cấu trúc Map mà không cần khởi tạo đối tượng nên nó là static.
+  static List<Note> fromList(List<Map<String, dynamic>> query) {
+    List<Note> items = List<Note>();
+    for (Map map in query) {
+      items.add(Note.fromMap(map));
+    }
+    return items;
+  }
+
+  /// Hàm tạo có tên, đây là một hàm tạo từ đối số là dữ liệu đưa vào
+  /// dưới dạng Map
+  Note.fromMap(Map data)
+      : id = data['id'],
+        title = data['title'],
+        desc = data['desc'],
+        isDeleted = data['isDeleted'] == 1 ? true : false;
+
+  /// Phương thức của đối tượng, nó cho phép tạo ra dữ liệu dạng Map từ
+  /// dữ liệu của một đối tượng ghi chú.
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'title': title,
+        'desc': desc,
+        'isDeleted': isDeleted ? 1 : 0,
+      };
 }
