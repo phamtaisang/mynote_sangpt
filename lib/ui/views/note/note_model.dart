6a60d5dfb2c79495962b84265537bef1596d4282
Sang Phạm Tài <phamtaisang97@gmail.com>
2020-11-30 09:17:13 +0700

    update note view model

--- lib/ui/views/note/note_model.dart
+++ lib/ui/views/note/note_model.dart
@@ -1,56 +1,60 @@
-import 'package:flutter/material.dart';
+import 'package:mynote/ui/views/note/note_repository.dart';
+import 'package:stacked/stacked.dart';
 
-class Note {
-  /// id tự sinh ra ngẫu nhiên
-  String id = UniqueKey()
-      .hashCode
-      .toUnsigned(20)
-      .toRadixString(16)
-      .padLeft(5, '0');
+import 'note_model.dart';
 
-  final String title;
-  final String desc;
-  bool isDeleted = false;
+/// Trạng thái của view
+enum NoteViewState { listView, itemView, insertView, updateView }
 
-  Note(this.title, this.desc);
+class NoteViewModel extends BaseViewModel {
+  final title = 'Note View Model';
 
-  /// Tên của bảng CSDL, nó nên được gán sẵn và có thể lấy ra từ data
-  /// model mà không cần khởi tạo nên nó là static để dễ sử dung.
-  static String get tableName => 'Notes';
+  /// Danh sách các bản ghi được load bất đồng bộ bên trong view model,
+  /// khi load thành công thì thông báo đến view để cập nhật trạng thái
+  var _items = <Note>[];
 
-  /// Chuỗi lệnh SQL để tạo bảng CSDL, nó nên được thiết lập để tạo bảng
-  /// trong CSDL mà không cần khởi tạo nên nó là static để dễ sử dụng.
-  static String get createTable =>
-      'CREATE TABLE $tableName(`id` TEXT PRIMARY KEY,'
-      ' `title` TEXT,'
-      ' `desc` TEXT,'
-      ' `isDeleted` INTEGER DEFAULT 0)';
+  /// ### Danh sách các bản ghi dùng để hiển thị trên ListView
+  /// Vì quá trình load items là bất đồng bộ nên phải tạo một getter
+  /// `get items => _items` để tránh xung đột
+  List<Note> get items => _items;
 
-  /// Phương thức này được thiết lập để tạo nên danh sách các ghi chú
-  /// được lấy về từ CSDL, nó được tạo dưới dạng danh sách các ghi chú
-  /// theo cấu trúc Map mà không cần khởi tạo đối tượng nên nó là static.
-  static List<Note> fromList(List<Map<String, dynamic>> query) {
-    List<Note> items = List<Note>();
-    for (Map map in query) {
-      items.add(Note.fromMap(map));
-    }
-    return items;
+  /// Trạng thái mặc định của view là listView, nó có thể thay đổi
+  /// bên trong view model
+  var _state = NoteViewState.listView;
+
+  /// Khi thay đổi trạng thái thì sẽ báo cho view biết để cập nhật
+  /// nên cần tạo một setter để vừa nhận giá trị vừa thông báo đến view
+  set state(value) {
+    // Cập nhật giá trị cho biến _state
+    _state = value;
+
+    // Thông báo cho view biết để cập nhật trạng thái của widget
+    notifyListeners();
   }
 
-  /// Hàm tạo có tên, đây là một hàm tạo từ đối số là dữ liệu đưa vào
-  /// dưới dạng Map
-  Note.fromMap(Map data)
-      : id = data['id'],
-        title = data['title'],
-        desc = data['desc'],
-        isDeleted = data['isDeleted'] == 1 ? true : false;
+  /// Cần có một getter để lấy ra trạng thái view cục bộ cho view
+  NoteViewState get state => _state;
 
-  /// Phương thức của đối tượng, nó cho phép tạo ra dữ liệu dạng Map từ
-  /// dữ liệu của một đối tượng ghi chú.
-  Map<String, dynamic> toMap() => {
-        'id': id,
-        'title': title,
-        'desc': desc,
-        'isDeleted': isDeleted ? 1 : 0,
-      };
+  ///
+  var repo = NoteRepository();
+
+  Future init() async {
+    return reloadItems();
+  }
+
+  Future reloadItems() async {
+    return repo.items().then((value) {
+      _items = value;
+      notifyListeners();
+    });
+  }
+
+  void addItem() {
+    var title = DateTime.now().millisecondsSinceEpoch.toString();
+    var desc = DateTime.now().toLocal().toString();
+    var item = Note(title, desc);
+    repo.insert(item).then((value) {
+      reloadItems();
+    });
+  }
 }
